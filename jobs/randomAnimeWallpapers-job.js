4bf871c78879dbe8924b42c47ef6d39cee356e09
Oregu7 <jekkicool@yandex.ru>
2017-10-18 11:39:28 +0300

    fix randAnimeWallpapers

--- jobs/randomAnimeWallpapers-job.js
+++ jobs/randomAnimeWallpapers-job.js
@@ -1,12 +1,12 @@
 const config = require('config');
+const rp = require('request-promise');
 const randAniWall = require('random-anime-wallpapers');
 const { createInlineKeyboard, newMessage } = require("telro").Utils;
 const choice = require('../core/choice');
 const jobFactory = require('../core/jobFactory');
 
-async function randomAnimeWallpapers(channel) {
-    //let genre = choice(config.get("Customer.jobs.randomAnimeWallpapers.genres"));
-    let wallpaper = (await randAniWall())[0];
+async function animeWallpapers(channel) {
+    let wallpaper = await getRandomAnimeWallpapers();
     let keyboard = createInlineKeyboard({ text: "\u{1F5BC}FULL", url: wallpaper.full });
     let caption = `НОВЫЕ ОБОИ\n#anidojo #anime_wallpapers #${wallpaper.id}`;
     let options = Object.assign({}, { caption }, keyboard);
@@ -15,4 +15,17 @@ async function randomAnimeWallpapers(channel) {
     channel.sendPhoto(wallpaper.thumb, options);
 }
 
-module.exports = jobFactory(randomAnimeWallpapers, config.get("Customer.jobs.randomAnimeWallpapers.time"));
+async function getRandomAnimeWallpapers() {
+    let wallpapers = await randAniWall();
+    for (let wallpaper of wallpapers) {
+        try {
+            let res = await rp.get(wallpaper.full);
+            return wallpaper;
+        } catch (err) {
+            continue;
+        }
+    }
+    return await getAnimeWallpapers();
+}
+
+module.exports = jobFactory(animeWallpapers, config.get("Customer.jobs.randomAnimeWallpapers.time"));
